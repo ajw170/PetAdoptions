9f5d75da48c48390e09fc695cc5bc04b47cbb970
Andrew Wood <wood.andrew9@gmail.com>
2020-10-07 20:35:29 -0400

    Final commit.

--- adoptions/models.py
+++ adoptions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Pet(models.Model):
 
-    # The list has 2-tuples, one for the DB value, the other for the display value
+    # The list has 2-tuples, one for the DB value, the other for the readable-display value
     SEX_CHOICES = [('M','Male'),('F','Female')]
 
     name = models.CharField(max_length=100)
@@ -12,15 +12,17 @@ class Pet(models.Model):
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField(blank=True)
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
-
     submission_date = models.DateTimeField()
 
     # Distinction between null and blank, a blank would be recorded as 0, a null is unknown
     age = models.IntegerField(null=True)
 
-    # associate a pet with vaccines.
+    # associate a pet with vaccines.  Some pets may not have vaccinations.
     vaccinations = models.ManyToManyField('Vaccine',blank=True)
 
 # Vaccines has Many-to-many relationships with Pet
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+
+from .models import Pet
+
+# Create your tests here.
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
+
+def pet_detail(request, pet_id):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet Not Found!')
+    return render(request, 'pet_detail.html', {'pet': pet})
